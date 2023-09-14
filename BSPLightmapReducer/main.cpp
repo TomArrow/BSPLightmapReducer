043553cd0e5d07083f0f3ff84b98f464f250e45c
Tom <tomarrowtomarrow@hotmail.com>
2023-09-14 14:17:40 +0200

    Linearize values before blending pixels for downscaling.

--- BSPLightmapReducer/main.cpp
+++ BSPLightmapReducer/main.cpp
@@ -1,6 +1,28 @@
 #include "common.h"
 
 
+enum resamplingMode_t {
+	RAW,
+	LINEAR,
+	LINEAR_OVERBRIGHT
+};
+
+static inline float linearizeSRGB(float n)
+{
+	return (n > 0.04045f ? (float)powf((n + 0.055) / 1.055, 2.4) : n / 12.92f);
+}
+static inline float delinearizeSRGB(float n)
+{
+	return n > 0.0031308f ? 1.055f * (float)powf(n, 1 / 2.4) - 0.055f : 12.92f * n;
+}
+
+inline float toLinear(byte value,bool overBright) {
+	return overBright ? linearizeSRGB((float)value*2.f/255.f) : linearizeSRGB((float)value / 255.f);
+}
+inline byte fromLinear(float value,bool overBright) {
+	return (byte)std::max(std::min((overBright ? delinearizeSRGB(value)*255.0f/2.0f : delinearizeSRGB(value) * 255.0f),255.f),0.0f);
+}
+
 int main(int argc, char** argv) {
 	int			i;
 	dheader_t* header;
@@ -11,6 +33,33 @@ int main(int argc, char** argv) {
 
 	const char* fileName = argv[1];
 	const char* fileNameOut = argv[2];
+
+	resamplingMode_t mode = LINEAR_OVERBRIGHT;
+
+	if (argc > 3) {
+		if (!stricmp(argv[3],"lin")) {
+			mode = LINEAR;
+		}
+		else if (!stricmp(argv[3],"raw")) {
+			mode = RAW;
+		}
+		else if (!stricmp(argv[3],"olin")) {
+			mode = LINEAR_OVERBRIGHT;
+		}
+	}
+	
+	switch (mode) {
+		case LINEAR:
+			std::cout << "Using linear pixel blending mode.\n";
+			break;
+		case RAW:
+			std::cout << "Using raw pixel blending mode.\n";
+			break;
+		case LINEAR_OVERBRIGHT:
+			std::cout << "Using overbright-linear pixel blending mode (default).\n";
+			break;
+	}
+	std::cout << "Use third parameter 'lin' for linear, 'raw' for raw and 'olin' for overbright-linear mode. Linear is best for games without overbright bits (jka), overbright-linear is best for games with overbright bits (jk2).\n";
 
 	int inputFileLength = FS_ReadFile(fileName, (void**)&buffer, qfalse);
 	if (!buffer) {
@@ -57,11 +106,28 @@ int main(int argc, char** argv) {
 			for (int x = 0; x < LIGHTMAP_WIDTH; x+=2) {
 				for (int y = 0; y < LIGHTMAP_HEIGHT; y+=2) {
 					byte* pixelPos = start + y * (LIGHTMAP_WIDTH * 3) + x * 3;
-					int r = (pixelPos[0]+pixelPos[3]+pixelPos[LIGHTMAP_WIDTH * 3]+pixelPos[LIGHTMAP_WIDTH * 3+3])/4; // Averaging the values of 4 pixels for one target pixel.
-					pixelPos++;
-					int g = (pixelPos[0]+pixelPos[3]+pixelPos[LIGHTMAP_WIDTH * 3]+pixelPos[LIGHTMAP_WIDTH * 3+3])/4; // Averaging the values of 4 pixels for one target pixel.
-					pixelPos++;
-					int b = (pixelPos[0]+pixelPos[3]+pixelPos[LIGHTMAP_WIDTH * 3]+pixelPos[LIGHTMAP_WIDTH * 3+3])/4; // Averaging the values of 4 pixels for one target pixel.
+					int r, g, b;
+					if (mode == RAW) {
+						r = (pixelPos[0] + pixelPos[3] + pixelPos[LIGHTMAP_WIDTH * 3] + pixelPos[LIGHTMAP_WIDTH * 3 + 3]) / 4; // Averaging the values of 4 pixels for one target pixel.
+						pixelPos++;
+						g = (pixelPos[0] + pixelPos[3] + pixelPos[LIGHTMAP_WIDTH * 3] + pixelPos[LIGHTMAP_WIDTH * 3 + 3]) / 4; // Averaging the values of 4 pixels for one target pixel.
+						pixelPos++;
+						b = (pixelPos[0] + pixelPos[3] + pixelPos[LIGHTMAP_WIDTH * 3] + pixelPos[LIGHTMAP_WIDTH * 3 + 3]) / 4; // Averaging the values of 4 pixels for one target pixel.
+					}
+					else if (mode == LINEAR) {
+						r = fromLinear((toLinear(pixelPos[0],false) + toLinear(pixelPos[3], false) + toLinear(pixelPos[LIGHTMAP_WIDTH * 3], false) + toLinear(pixelPos[LIGHTMAP_WIDTH * 3 + 3], false)) / 4.f,false); // Averaging the 4 linearized float values for one target pixel
+						pixelPos++;
+						g = fromLinear((toLinear(pixelPos[0], false) + toLinear(pixelPos[3], false) + toLinear(pixelPos[LIGHTMAP_WIDTH * 3], false) + toLinear(pixelPos[LIGHTMAP_WIDTH * 3 + 3], false)) / 4.f, false); // Averaging the 4 linearized float values for one target pixel
+						pixelPos++;
+						b = fromLinear((toLinear(pixelPos[0], false) + toLinear(pixelPos[3], false) + toLinear(pixelPos[LIGHTMAP_WIDTH * 3], false) + toLinear(pixelPos[LIGHTMAP_WIDTH * 3 + 3], false)) / 4.f, false); // Averaging the 4 linearized float values for one target pixel
+					}
+					else if (mode == LINEAR_OVERBRIGHT) {
+						r = fromLinear((toLinear(pixelPos[0], true) + toLinear(pixelPos[3], true) + toLinear(pixelPos[LIGHTMAP_WIDTH * 3], true) + toLinear(pixelPos[LIGHTMAP_WIDTH * 3 + 3], true)) / 4.f,true); // Averaging the 4 linearized float values for one target pixel
+						pixelPos++;
+						g = fromLinear((toLinear(pixelPos[0], true) + toLinear(pixelPos[3], true) + toLinear(pixelPos[LIGHTMAP_WIDTH * 3], true) + toLinear(pixelPos[LIGHTMAP_WIDTH * 3 + 3], true)) / 4.f, true); // Averaging the 4 linearized float values for one target pixel
+						pixelPos++;
+						b = fromLinear((toLinear(pixelPos[0], true) + toLinear(pixelPos[3], true) + toLinear(pixelPos[LIGHTMAP_WIDTH * 3], true) + toLinear(pixelPos[LIGHTMAP_WIDTH * 3 + 3], true)) / 4.f, true); // Averaging the 4 linearized float values for one target pixel
+					}
 
 					int targetX = targetXOffset + x / 2;
 					int targetY = targetYOffset + y / 2;
